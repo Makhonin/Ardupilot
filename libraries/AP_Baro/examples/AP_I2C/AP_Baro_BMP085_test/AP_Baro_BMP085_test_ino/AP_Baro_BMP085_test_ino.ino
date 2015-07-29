f55b38c3e35d383d84a6601d7a32deead493ba77
Makhonin Oleg <MakhoninOM@gmail.com>
2015-07-29 17:22:15 +0300

    Working servo-s

--- libraries/AP_Baro/examples/AP_I2C/AP_Baro_BMP085_test/AP_Baro_BMP085_test_ino/AP_Baro_BMP085_test_ino.ino
+++ libraries/AP_Baro/examples/AP_I2C/AP_Baro_BMP085_test/AP_Baro_BMP085_test_ino/AP_Baro_BMP085_test_ino.ino
@@ -29,54 +29,138 @@ const AP_HAL::HAL& hal = AP_HAL_BOARD_DRIVER;
 #define PCA9685_I2C_ADDRESS 0x38
 #define PCA9685_I2C_ADDRESS2 0x38
 #define PCA9685_I2C_BASE_ADDRESS 0x40
-#define PCA9685_MODE1 0x00 // location for Mode1 register address
-#define PCA9685_MODE2 0x01 // location for Mode2 reigster address
-#define PCA9685_LED0 0x06 // location for start of LED0 registers
 
-#define I2C_24C02 0x50
-#define PCA9685_I2C_BASE_ADDRESS 0x40
-#define PCA9685_MODE1 0x00 // location for Mode1 register address
-#define PCA9685_MODE2 0x01 // location for Mode2 reigster address
-#define PCA9685_LED0 0x06 // location for start of LED0 registers
+#define PCA9685_RESTART 0x80 // 0b10000000
+#define PCA9685_EXTCLK 0x40 // 0b01000000
+#define PCA9685_AI 0x20 // 0b00100000
+#define PCA9685_SLEEP 0x10 // 0b00010000
+#define PCA9685_SUB1 0x8 // 0b00001000
+#define PCA9685_SUB2 0x4 // 0b00000100
+#define PCA9685_SUB3 0x2 // 0b00000010
+#define PCA9685_ALLCALL 0x1 // 0b00000001
+
+//MODE 2
+#define PCA9685_INVRT 0x10 // 0b00010000
+#define PCA9685_OCH 0x8 // 0b00001000
+#define PCA9685_OUTDRV 0x4 // 0b00000100
+#define PCA9685_OUTNE1 0x2 // 0b00000010
+#define PCA9685_OUTNE2 0x1 // 0b00000001
+
+#define PCA9685_PRESCALER 0xFE
+#define PCA9685_MODE1 0x00
+#define PCA9685_MODE2 0x01
+
+#define CALC_FREQ(x) 25000000/4096/x-1
+
+#define LED_ON_L(x) 0x6+4*x
+#define LED_ON_H(x) 0x7+4*x
+#define LED_OFF_L(x) 0x8+4*x
+#define LED_OFF_H(x) 0x9+4*x
+
+#define LOW_PART(x) x&0xFF
+#define HIGH_PART(x) x>>8
 
 
 uint32_t timer;
 uint8_t i2cres=0;
 uint8_t i2cadress;
 
+uint16_t cnt;
+uint16_t pwm1,pwm2,pwm3;
+int16_t dird=1;
+
+uint8_t writeRegg2(uint8_t reg,uint8_t data)
+{
+  if (hal.i2c->writeRegister(PCA9685_I2C_BASE_ADDRESS , reg,data)!=0)
+  {
+    hal.console->println("PCA9685 I2C connection error.");
+  }
+}
+
+void initPCA9685()
+{
+  uint16_t led0on = 0x409;
+  uint16_t led0off = 0x1228;
+  writeRegg2(PCA9685_MODE1,0x10);
+  //uint32_t fr = 500000/4096-1;
+  writeRegg2(PCA9685_PRESCALER,133);
+  writeRegg2(PCA9685_MODE1,0x1);
+  writeRegg2(PCA9685_MODE2,0x14&(~PCA9685_INVRT));
+
+  writeRegg2(LED_ON_L(0),LOW_PART(led0on));
+  writeRegg2(LED_ON_H(0),HIGH_PART(led0on));
+  writeRegg2(LED_OFF_L(0),LOW_PART(led0off));
+  writeRegg2(LED_OFF_H(0),HIGH_PART(led0off));
+}
+
+void setPWM(uint8_t num, uint16_t on,uint16_t off)
+{
+  writeRegg2(LED_ON_L(num),LOW_PART(on));
+  writeRegg2(LED_ON_H(num),HIGH_PART(on));
+  writeRegg2(LED_OFF_L(num),LOW_PART(off));
+  writeRegg2(LED_OFF_H(num),HIGH_PART(off));
+}
+
+void setServo(uint8_t num, uint16_t pwm) //angle from 1000 to 2000
+{
+  // 4096 - 20 msecs
+  // 4096/20 - 1 msec
+  // 4096/10 - 2 msec
+  uint32_t pwm2d = 4096*((uint32_t)pwm);
+  pwm2d/=20000;
+  hal.console->printf("PWM2: %lu",pwm2d);
+  writeRegg2(LED_ON_L(num),LOW_PART(0));
+  writeRegg2(LED_ON_H(num),HIGH_PART(0));
+  writeRegg2(LED_OFF_L(num),LOW_PART(pwm2d));
+  writeRegg2(LED_OFF_H(num),HIGH_PART(pwm2d));
+}
+
 void setup()
 {
-    
-      hal.console->println("ArduPilot Mega BMP085 library test");
-    hal.console->println("Initialising PCA9685...");
 
-    hal.scheduler->delay(100);
-    AP_HAL::Semaphore* i2c_sem = hal.i2c->get_semaphore();
+  hal.console->println("PCA9685 library test");
+  hal.console->println("Initialising PCA9685...");
 
-    // take i2c bus sempahore
-    if (!i2c_sem->take(HAL_SEMAPHORE_BLOCK_FOREVER))
-        return;
-        
-    i2cadress =  PCA9685_I2C_BASE_ADDRESS;  
+  hal.scheduler->delay(100);
+  AP_HAL::Semaphore* i2c_sem = hal.i2c->get_semaphore();
+
+  // take i2c bus sempahore
+  if (!i2c_sem->take(HAL_SEMAPHORE_BLOCK_FOREVER))
+    return;
+
+  dird=5;
+  cnt=1;
+  pwm1=1;
+  pwm2=1;
+  pwm3=1;
+  initPCA9685();
 }
 
 void loop()
 {
- 
-	 
-	if (hal.i2c->writeRegister(i2cadress, 0x0,0)!=0)
-	{
-		hal.console->println("PCA9685 I2C connection error.");
-	}
-        hal.console->println("PCA9685 MODE1.");
-	
-        if (hal.i2c->writeRegister(i2cadress, 0x1,0x4)!=0)
-	{
-		hal.console->println("PCA9685 I2C connection error.");
-	}
-        hal.console->println("PCA9685 MODE2.");
+  cnt+=dird;
+  pwm1+=pwm2;
+  if (cnt>1000)
+    dird=-dird;
+  if (cnt<6)
+    dird=-dird;
+  if (pwm1>1000)
+    pwm2=-pwm2;
+  if (pwm1<1)
+    pwm2=-pwm2;
+  //setServo(0,0x0,cnt);
+  hal.console->printf("Servo1 PWM %lu",(uint32_t)(1000+cnt));
+  setServo(1,1000+cnt); //cnt from 0 to 4096
+  hal.console->printf("Servo2 PWM %lu",(uint32_t)(1000+1000*(pwm1&0x08)/0x08));
+  setServo(2,1000+1000*(pwm1&0x08)/0x08);
+  hal.console->printf("Servo3 PWM %lu",(uint32_t)(1000+1000*(pwm1&0x10)/0x10));
+  setServo(3,1000+1000*(pwm1&0x10)/0x10);
+  hal.console->printf("Servo4 PWM %lu",(uint32_t)(1000+1000*(pwm1&0x20)/0x20));
+  setServo(4,1000+1000*(pwm1&0x20)/0x20);
 
-        hal.console->println("Again.");
+  //hal.console->println("Again.");
+  hal.scheduler->delay(100);
 }
 
 AP_HAL_MAIN();
+
